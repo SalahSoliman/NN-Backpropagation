3d4351b572127dd2b7a8b4225bffee175f2de1d1
“SalahSoliman” <salahsoliman96@gmail.com>
2020-11-14 16:21:12 +0200

    MNIST added

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -74,15 +74,15 @@ class NeuralNetwork:
         return loss
 
 
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([[0], [1], [1], [0]])
+# X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+# y = np.array([[0], [1], [1], [0]])
 
-nn = NeuralNetwork([2,3,1], alpha=0.5)
-nn.fit(X, y, epochs=20000)
+# nn = NeuralNetwork([2,3,1], alpha=0.5)
+# nn.fit(X, y, epochs=20000)
 
-for (x, target) in zip (X, y):
-    pred = nn.predict(x)[0][0]
-    step = 1 if pred > 0.5 else 0
-    print("[INFO] data={}, ground_truth={}, pred={:.7f}, step={}".format(x, target[0], pred, step))
+# for (x, target) in zip (X, y):
+#     pred = nn.predict(x)[0][0]
+#     step = 1 if pred > 0.5 else 0
+#     print("[INFO] data={}, ground_truth={}, pred={:.7f}, step={}".format(x, target[0], pred, step))
 
-print(nn)
+# print(nn)
